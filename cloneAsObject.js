e88eef8670014d88a5137b10873b92b4fe523f82
Otis Ma <littlehorseboy@gmail.com>
2019-11-28 12:05:34 +0800

    feat: list.html 列出 list.json

---
+++ cloneAsObject.js
@@ -0,0 +1,11 @@
+function cloneAsObject(obj) {
+  if (obj === null || !(obj instanceof Object)) {
+    return obj;
+  }
+  var temp = (obj instanceof Array) ? [] : {};
+  // ReSharper disable once MissingHasOwnPropertyInForeach
+  for (var key in obj) {
+    temp[key] = cloneAsObject(obj[key]);
+  }
+  return temp;
+}
